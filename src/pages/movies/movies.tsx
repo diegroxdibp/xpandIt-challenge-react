b4bcaf9a608daa76259a9f7744c75a40769a23e9
DiegoPonte <diego.ponte@xpand-it.com>
2022-02-22 10:38:03 +0000

    ranking toggling on button

--- src/pages/movies/movies.tsx
+++ src/pages/movies/movies.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
 /* eslint-disable no-tabs */
 import { Pill } from '../../components/pill/pill'
 import { MovieGridHeader } from '../../components/movie-grid'
@@ -6,8 +7,6 @@ import { DividerHeader } from '../../components/divider'
 import { MovieRankingState } from '../../models/movie-ranking-status'
 import { useState } from 'react'
 import { MovieRankingList } from '../../components/movie-list/movie-list'
-// import { useState } from 'react'
-// import { PillState } from '../../hooks/pill-status'
 
 export const MovieRanking = () => {
   const [movieRankingState, useMovieRankingState] = useState<MovieRankingState>(
@@ -16,13 +15,21 @@ export const MovieRanking = () => {
   return (
     <MoviePageBody>
       <h1>Movie ranking</h1>
+      //TODO REFACTOR
       <PillsWrapper>
-        <div onClick={() => useMovieRankingState(MovieRankingState.top10rev)}>
+        <div
+          onClick={() => {
+            if (movieRankingState === MovieRankingState.default) {
+              useMovieRankingState(MovieRankingState.top10rev)
+            } else {
+              useMovieRankingState(MovieRankingState.default)
+            }
+          }}
+        >
           <Pill title="Top 10 Revenue"></Pill>
         </div>
         <Pill title="Top 10 Revenue per Year"></Pill>
       </PillsWrapper>
-
       <MovieGridHeader>
         <span>RANKING</span>
         <span className="title">TITLE</span>
@@ -31,10 +38,7 @@ export const MovieRanking = () => {
         <span></span>
       </MovieGridHeader>
       <DividerHeader />
-
       <MovieRankingList state={movieRankingState}></MovieRankingList>
-
-      {/* <Link to="/pageB">Navigate to Page B</Link> */}
     </MoviePageBody>
   )
 }
